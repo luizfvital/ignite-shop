e1c4993749a7a7584ba03df6c97959302fbb9f23
Luiz Fernando Vital <luizfvital@gmail.com>
2023-04-25 21:03:33 +0100

    Add product page with getStaticProps

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,87 @@
-import { useRouter } from 'next/router'
+import { GetStaticProps, GetStaticPaths } from "next"
+import Image from "next/image";
+import Stripe from "stripe";
+import { stripe } from "../../lib/stripe";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+import {useRouter} from 'next/router';
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
+
+export default function Product({product}: ProductProps) {
+
+  const { isFallback } = useRouter()
+
+  if (isFallback) {
+    return <p>Loading...</p>
+  }
+
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { id: 'prod_MLH5Wy0Y97hDAC' }
+      }
+    ],
+    fallback: true,
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  if (!params || !params.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount ? price.unit_amount / 100 : 0),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1 // 1 hours
+  }
 }
